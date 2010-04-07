56dc44c0dcb62d9191ea82fecb01184808b9c9f8
bozhidar.bozhanov@gmail.com <bozhidar.bozhanov@gmail.com@5587b286-15cb-11df-8cda-5d4afe61fefd>
2010-04-07 19:28:21 +0000

    organized imports

--- trunk/blink/src/main/java/org/blink/utils/BlinkBeanUtils.java
+++ trunk/blink/src/main/java/org/blink/utils/BlinkBeanUtils.java
@@ -1,18 +1,5 @@
 package org.blink.utils;
 
-import java.util.Set;
-
-import javax.enterprise.inject.spi.AnnotatedConstructor;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.InjectionPoint;
-import javax.inject.Inject;
-
-import org.blink.exceptions.DefinitionException;
-import org.blink.types.BlinkAnnotatedType;
-import org.blink.types.injectionpoints.InjectionPointImpl;
-
-
 public final class BlinkBeanUtils {
-
 
 }
