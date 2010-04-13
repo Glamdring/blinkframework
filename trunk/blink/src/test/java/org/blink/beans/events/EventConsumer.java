1d2a05a8195a825feaf0c4b366f070e920bc3254
bozhidar.bozhanov@gmail.com <bozhidar.bozhanov@gmail.com@5587b286-15cb-11df-8cda-5d4afe61fefd>
2010-04-13 17:19:40 +0000

    events tests

--- trunk/blink/src/test/java/org/blink/beans/events/EventConsumer.java
+++ trunk/blink/src/test/java/org/blink/beans/events/EventConsumer.java
@@ -3,13 +3,24 @@ package org.blink.beans.events;
 import javax.enterprise.event.Observes;
 import javax.inject.Named;
 
+import org.blink.beans.injection.First;
+
 @Named
 public class EventConsumer {
 
     private boolean eventHandled;
+    private boolean unmachingQualifiersEventHandled;
+    private boolean noQualifiersEventHandled;
 
     public void observes(@Observes @EventQualifier TestEvent event) {
         eventHandled = true;
+    }
+
+    public void observesUnmatching(@Observes @First TestEvent event) {
+        unmachingQualifiersEventHandled = true;
+    }
+    public void observesNoQualifiers(@Observes TestEvent event) {
+        noQualifiersEventHandled = true;
     }
 
     public boolean isEventHandled() {
@@ -18,5 +29,22 @@ public class EventConsumer {
 
     public void setEventHandled(boolean eventHandled) {
         this.eventHandled = eventHandled;
+    }
+
+    public boolean isUnmachingQualifiersEventHandled() {
+        return unmachingQualifiersEventHandled;
+    }
+
+    public void setUnmachingQualifiersEventHandled(
+            boolean unmachingQualifiersEventHandled) {
+        this.unmachingQualifiersEventHandled = unmachingQualifiersEventHandled;
+    }
+
+    public boolean isNoQualifiersEventHandled() {
+        return noQualifiersEventHandled;
+    }
+
+    public void setNoQualifiersEventHandled(boolean noQualifiersEventHandled) {
+        this.noQualifiersEventHandled = noQualifiersEventHandled;
     }
 }
